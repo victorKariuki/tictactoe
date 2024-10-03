12c9acbca3f3b3738f520a90a16483446d6620a7
Victor Kariuki <vikgachewa@hotmail.com>
2024-10-03 21:45:11 +0300

    ```text refactor: Update ecosystem.config.js
    Refactor the ecosystem.config.js file to improve readability and maintainability. The changes include adding a conditional statement to set the app name based on the environment, updating the script path, and configuring the number of instances and execution mode. Additionally, the log date format has been specified.

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+const appName =
+  process.env.NODE_ENV === "production"
+    ? "tictactoe-production"
+    : "tictactoe-development";
+
+module.exports = {
+  apps: [
+    {
+      name: appName,
+      script: "./bin/www.js",
+      instances: 4,
+      exec_mode: "cluster",
+      env_production: {
+        NODE_ENV: process.env.NODE_ENV,
+      },
+      env_development: {
+        NODE_ENV: process.env.NODE_ENV,
+      },
+      watch: false,
+      "log_date_format": "YYYY-MM-DD HH:mm Z"
+    },
+  ],
+};
